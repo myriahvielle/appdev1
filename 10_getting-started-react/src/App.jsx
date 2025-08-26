3ffc912b81242609f95042efb6d790795612c29b
unknown <myriahvielle619@gmail.com>
2025-08-26 20:52:19 +0800

    Activity #10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", padding: "20px" }}>
+      <h1>About Me</h1>
+      <h2>Name: Myriah Vielle Silverio</h2>
+      <h3>Course & Year: BS Information Systems - 3rd Year</h3>
+      <p><strong>Fun Fact:</strong> I love cooking and creating budget-friendly healthy meals!</p>
+      <p>
+        I want to learn React because I aim to explore on 
+        how to efficiently make an interactive and easier user interfaces 
+        that caters my needs, especially for my personal business.
+      </p>
+    </div>
+  );
+}
+
+export default App;
+
