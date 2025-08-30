197f43c160b813b9263213ffa4e5774762845922
unknown <myriahvielle619@gmail.com>
2025-08-30 13:52:15 +0800

    on-going 07_dom.html

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,30 @@
+let myName = "Myriah Vielle";
+let age = 23;
+let isStudent = true;
+
+
+//Print my name, age and a student
+console.log("Name:", myName);
+console.log("Age:", age);
+console.log("Is student:", isStudent);
+
+//Check typeof
+console.log("Type of myName:", typeof myName); 
+console.log("Type of age:", typeof age);
+console.log("Type of isStudent:", typeof isStudent);
+
+//Arithmetic operations 
+let num1 = 10;
+let num2 = 2; 
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+
+//Difference of == and ===
+console.log('"5" == 5:', "5"== 5);
+console.log('"5" === 5:', "5"=== 5);
+
+//The use of == only compares the value
+//But with ===, it compares the value and the data type.
---
+++ 11_javascript-refresher/06_control_structure.js
@@ -0,0 +1,29 @@
+let score = 85; 
+
+if (score >=90){
+    console.log("Grade: A");
+}
+else if (score >=80){
+    console.log("Grade: B");
+}
+else if (score >=70){
+    console.log("Grade: C");
+}
+else{ 
+    console.log("Grade: F");
+}
+
+//for loop 
+console.log("\nCounting from 1 to 5:");
+for (let i = 1; i <= 5; i++){
+    console.log(i);
+}
+
+//while loop
+console.log("\nWhile Loop: ")
+let count = 0; 
+
+while (count < 3){
+    console.log("Hello");
+    count++; 
+}
