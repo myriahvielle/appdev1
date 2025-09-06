cf1248b8abd7e5a5d237cba3f8a5b042b85cd35a
unknown <myriahvielle619@gmail.com>
2025-09-06 14:14:11 +0800

    Add base syntax activity

---
+++ 11_javascript-refresher/01_base_syntax.js
@@ -0,0 +1,10 @@
+//Print Hello, Javascript
+console.log("Hello Javascript")
+
+// uppercase 
+let myName = "MYRIAH VIELLE";
+console.log(myName)
+
+// lowercase 
+let myname = "myriah vielle";
+console.log(myname)
