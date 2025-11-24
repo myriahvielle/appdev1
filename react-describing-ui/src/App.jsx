533931999a87503d43815489c27667c21188c4c5
unknown <myriahvielle619@gmail.com>
2025-11-24 13:18:33 +0800

    Done with why understanding your UI as trees is useful

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -5,17 +5,4 @@ import './App.css'
 import { people } from './data.jsx';
 import { getImageUrl } from './utils.jsx';
 
-function Cup({ guest }) {
-  return <h2>Tea cup for guest #{guest}</h2>;
-}
-
-export default function TeaSet() {
-  return (
-    <>
-      <Cup guest={1} />
-      <Cup guest={2} />
-      <Cup guest={3} />
-    </>
-  );
-}
 
