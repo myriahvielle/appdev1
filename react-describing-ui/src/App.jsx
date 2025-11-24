55ef8663db9d4b379309559d876636396163cabd
unknown <myriahvielle619@gmail.com>
2025-11-24 13:13:39 +0800

    Done with how to conditionally render components

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -4,41 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { getImageUrl } from './utils.jsx'; 
 
-export default function Profile() {
+function Item({ name, isPacked }) {
   return (
-    <Card>
-      <Avatar
-        size={100}
-        person={{
-          name: 'Katsuko Saruhashi',
-          imageId: 'YfeOqp2'
-        }}
-      />
-    </Card>
+    <li className="item">
+      {name} {isPacked && '✅'}
+    </li>
   );
 }
 
-function Avatar({ person, size }) {
+export default function PackingList() {
   return (
-    <img
-      className="avatar"
-      src={getImageUrl(person)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+          isPacked={true}
+          name="Space suit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
+      </ul>
+    </section>
   );
 }
-
-function Card({ children }) {
-  return (
-    <div className="card">
-      {children}
-    </div>
-  );
-}
-
-
-
-
-
