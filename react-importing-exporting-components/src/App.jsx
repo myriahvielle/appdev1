f4d76b1f8f86db971cb30b8dd7255b53ac97e1e9
unknown <myriahvielle619@gmail.com>
2025-11-28 21:47:35 +0800

    Done with exporting and importing a component

---
+++ react-importing-exporting-components/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Gallery from './Gallery.js';
+
+export default function App() {
+  return (
+    <Gallery />
+  );
+}
+
