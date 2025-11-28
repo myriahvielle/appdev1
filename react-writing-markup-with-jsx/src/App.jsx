f73901a69deca8eb01705c4e4f21cecac756d5ad
unknown <myriahvielle619@gmail.com>
2025-11-28 23:04:56 +0800

    Done with the rules of JSX

--- react-writing-markup-with-jsx/src/App.jsx
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -1,20 +1,17 @@
-function App() {
-
-
+export default function TodoList() {
   return (
-    <h1>Hedy Lamarr's Todos</h1>
-<img 
-  src="https://i.imgur.com/yXOvdOSs.jpg" 
-  alt="Hedy Lamarr" 
-  class="photo"
->
-<ul>
-    <li>Invent new traffic lights
-    <li>Rehearse a movie scene
-    <li>Improve the spectrum technology
-</ul>
-
-  )
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img 
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr" 
+        className="photo" 
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
 }
-
-export default App
