a1501d02e8686588563685fe2294ab59dbe011b9
unknown <myriahvielle619@gmail.com>
2025-11-28 22:19:39 +0800

    Done with set up React Router section

--- react-todo-app-useeffect-router-redux/src/main.jsx
+++ react-todo-app-useeffect-router-redux/src/main.jsx
@@ -4,3 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+)
